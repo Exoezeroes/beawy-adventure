db7bd324b7108a1e523376bfa400a8cf3c4d2997
ZeroRaxo <marvellgiovano@gmail.com>
2023-01-18 21:11:06 +0700

    Revert "Revert "Implemented Basic Main Menu""
    This reverts commit e35379bce7a2139ee9bdf642baf1797e0c801be0.

---
+++ BeawyAdventure2D/Assets/Scripts/MenuScripts.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScripts : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Applicated Quitted!");
+    }
+}
